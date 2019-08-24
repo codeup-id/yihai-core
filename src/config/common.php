d316d6b8a35c92276b63328d1e40c14d072d2363
Upik Saleh <upxsal@gmail.com>
2019-08-24 12:33:01 +0800

    move components to web

--- src/config/common.php
+++ src/config/common.php
@@ -44,53 +44,5 @@ return [
         'settings' => [
             'class' => 'yihai\core\base\Settings'
         ],
-        'reports' => [
-            'class' => 'yihai\core\report\ReportComponent'
-        ],
-        'fileManager' => [
-            'class' => 'yihai\core\extension\FileManager',
-            'connectOptions' => [
-            ],
-            'volumes' => [
-                [
-                    'class' => 'yihai\core\extension\elfinder\volume\Local',
-                    'id' => 'root',
-                    'position' => 0,
-                    'path' => '.',
-                    'name' => 'ROOT (Superuser)',
-                    'plugin' => [
-                        'Sluggable' => [
-
-                        ]
-                    ],
-                    'access_read' => [\yihai\core\rbac\RbacHelper::roleRoleName('superuser')],
-                    'access_write' => [\yihai\core\rbac\RbacHelper::roleRoleName('superuser')],
-                ],
-                [
-                    'class' => 'yihai\core\extension\elfinder\volume\UserPath',
-                    'id' => 'home-user',
-                    'position' => 0,
-                    'path' => 'users/{id}',
-                    'name' => 'Home',
-                    'plugin' => [
-                        'Sluggable' => [
-
-                        ]
-                    ],
-//                    'access_read' => [],
-//                    'access_write' => [],
-//                    'watermark' => [
-//                        'source'         => '/Users/upik/www/codeup/yihai-cms/cms/web/assets/49572765/default_avatar.png', // Path to Water mark image
-//                        'marginRight'    => 5,          // Margin right pixel
-//                        'marginBottom'   => 5,          // Margin bottom pixel
-//                        'quality'        => 95,         // JPEG image save quality
-//                        'transparency'   => 70,         // Water mark image transparency ( other than PNG )
-//                        'targetType'     => IMG_GIF|IMG_JPG|IMG_PNG|IMG_WBMP, // Target image formats ( bit-field )
-//                        'targetMinPixel' => 200         // Target image minimum pixel size
-//
-//                    ]
-                ],
-            ],
-        ]
     ]
 ];
