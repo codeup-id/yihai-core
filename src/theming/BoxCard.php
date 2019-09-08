8af58abdf58aba7ed6463545445c43c5c6b65e0e
Upik Saleh <upxsal@gmail.com>
2019-09-08 21:29:03 +0800

    fix, footer class

--- src/theming/BoxCard.php
+++ src/theming/BoxCard.php
@@ -3,7 +3,7 @@
  *  Yihai
  *
  *  Copyright (c) 2019, CodeUP.
- *  @author  Upik Saleh <upik@codeup.id>
+ * @author  Upik Saleh <upik@codeup.id>
  */
 
 namespace yihai\core\theming;
@@ -38,14 +38,15 @@ class BoxCard extends BaseWidget
     public $afterBody;
 
     // --------------------------------------------------------------------
+
     /**
      * Initializes the widget.
      */
     public function init()
     {
-        Html::addCssClass($this->options, ['box', 'box-'.$this->type, ($this->isCollapsed ? 'collapsed-box':'')]);
+        Html::addCssClass($this->options, ['box', 'box-' . $this->type, ($this->isCollapsed ? 'collapsed-box' : '')]);
         echo Html::beginTag('div', $this->options);
-        if($this->header) {
+        if ($this->header) {
             Html::addCssClass($this->headerOptions, 'box-header' . ($this->headerBorder ? ' with-border' : ''));
             echo Html::beginTag('div', $this->headerOptions);
             echo Html::tag('h3', $this->title, ['class' => 'box-title']);
@@ -60,46 +61,49 @@ class BoxCard extends BaseWidget
     public function run()
     {
         echo Html::endTag('div');   //body
-        if($this->afterBody)
+        if ($this->afterBody)
             echo $this->afterBody;
-        if($this->loading){
-            echo Html::tag('div', Html::icon(['refresh','spin']), ['class'=>'overlay']);
+        if ($this->loading) {
+            echo Html::tag('div', Html::icon(['refresh', 'spin']), ['class' => 'overlay']);
         }
         $this->_initFooter();
         echo Html::endTag('div');   // box
     }
 
-    private function _initHeaderTools(){
+    private function _initHeaderTools()
+    {
         Html::addCssClass($this->headerToolsOptions, ['box-tools pull-right']);
         echo Html::beginTag('div', $this->headerToolsOptions);
-        if(in_array('collapse',$this->tools_order)){
+        if (in_array('collapse', $this->tools_order)) {
             $this->headerTools['collapse'] = Html::button(
-                Html::icon(($this->isCollapsed?'plus':'minus')),
+                Html::icon(($this->isCollapsed ? 'plus' : 'minus')),
                 [
-                    'class'=>'btn btn-box-tool',
-                    'data-widget'=>'collapse'
+                    'class' => 'btn btn-box-tool',
+                    'data-widget' => 'collapse'
                 ]
             );
         }
-        if(in_array('remove',$this->tools_order)){
+        if (in_array('remove', $this->tools_order)) {
             $this->headerTools['remove'] = Html::button(
                 Html::icon('times'),
                 [
-                    'class'=>'btn btn-box-tool',
-                    'data-widget'=>'remove'
+                    'class' => 'btn btn-box-tool',
+                    'data-widget' => 'remove'
                 ]
             );
         }
-        foreach($this->tools_order as $name){
-            if(isset($this->headerTools[$name]))
+        foreach ($this->tools_order as $name) {
+            if (isset($this->headerTools[$name]))
                 echo $this->headerTools[$name];
         }
         echo Html::endTag('div');
     }
 
-    private function _initFooter(){
-        if($this->footer){
-            echo Html::beginTag('div',['class'=>'box-footer']);
+    private function _initFooter()
+    {
+        if ($this->footer) {
+            Html::addCssClass($this->footerOptions, 'box-footer');
+            echo Html::beginTag('div', $this->footerOptions);
             echo $this->footerContent;
             echo Html::endTag('div');
         }
