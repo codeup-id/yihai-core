8c14a9dd9de68f6db8b3f22ed0e13db8ad4117a3
upiksaleh <upxsal@gmail.com>
2019-09-13 11:20:33 +0800

    remove unused settings

--- src/themes/defyihai/views/_layouts/backend.php
+++ src/themes/defyihai/views/_layouts/backend.php
@@ -244,11 +244,7 @@ $mainClass = Yihai::$app->controller->module->id . '-'.Yihai::$app->controller->
                                 ?>
                             </select>
                     </div>
-                <div class="form-group" id="skins-list">
-                </div>
-                <span id="zoom_in">in</span>
-                <span id="zoom_out">out</span>
-                <span id="zoom_reset">reset</span>
+                <div class="form-group" id="skins-list"></div>
                 <button class="btn btn-primary btn-block"><?=Yihai::t('yihai','Simpan')?></button>
                 <?=Html::endForm();?>
             </div>
@@ -256,39 +252,6 @@ $mainClass = Yihai::$app->controller->module->id . '-'.Yihai::$app->controller->
     </aside>
 </div>
 <?php
-$this->registerJs('var zoom_level=100;
-
-        // Click events
-        $(\'#zoom_in\').click(function() { zoom_page(10, $(this)) });
-        $(\'#zoom_out\').click(function() { zoom_page(-10, $(this)) });
-        $(\'#zoom_reset\').click(function() { zoom_page(0, $(this)) });
-
-        // Zoom function
-        function zoom_page(step, trigger)
-        {
-            // Zoom just to steps in or out
-            if(zoom_level>=120 && step>0 || zoom_level<=80 && step<0) return;
-
-            // Set / reset zoom
-            if(step==0) zoom_level=100;
-            else zoom_level=zoom_level+step;
-
-            // Set page zoom via CSS
-            $(\'body\').css({
-                transform: \'scale(\'+(zoom_level/100)+\')\', // set zoom
-                transformOrigin: \'50% 0\' // set transform scale base
-            });
-
-            // Adjust page to zoom width
-            if(zoom_level>100) $(\'body\').css({ width: (zoom_level)+\'%\' });
-            else $(\'body\').css({ width: \'100%\' });
-
-            // Activate / deaktivate trigger (use CSS to make them look different)
-            if(zoom_level>=120 || zoom_level<=80) trigger.addClass(\'disabled\');
-            else trigger.parents(\'ul\').find(\'.disabled\').removeClass(\'disabled\');
-            if(zoom_level!=100) $(\'#zoom_reset\').removeClass(\'disabled\');
-            else $(\'#zoom_reset\').addClass(\'disabled\');
-        }');
 $overlay = '<div class="overlay"><i class="fa fa-spin fa-spinner"></i></div>';
 Modal::begin([
     'id' => 'yihai-crud-basemodal',
